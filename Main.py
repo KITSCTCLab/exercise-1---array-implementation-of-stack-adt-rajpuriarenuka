07f1c71c009048f2df1948282d635174e2e83ecc
rajpuriarenuka <91603618+rajpuriarenuka@users.noreply.github.com>
2022-07-26 12:34:22 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,23 +3,28 @@ class Stack:
     def __init__(self, size):
         self.items = []
         self.size = size
+        self.top = -1
 
     def is_empty(self):
-        # Write code here
+        return self.top == -1
 
     def is_full(self):
-        # Write code here
+        return self.top == self.size - 1
 
     def push(self, data):
         if not self.is_full():
-            # Write code here
+            self.items.append(data)
+            self.top += 1
 
     def pop(self):
         if not self.is_empty():
-            # Write code here
+            x = self.items.pop(self.top)
+            print("Element popped: ", x)
+            self.top -= 1
 
     def status(self):
-        # Write code here
+        print(self.items)
+
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
